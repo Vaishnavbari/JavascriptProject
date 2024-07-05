66f6dc6e3eb688c6827dce86a2a1d9fe3d4d57cb
vaishnav <vaishnav@empiricinfotech.com>
2024-07-05 17:42:55 +0530

    add api to crated dynamic element

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-  path("home/", views.HomeView.as_view(), name="HomePage")
+  path("home/", views.HomeView.as_view(), name="HomePage"),
+  path("module/<int:count>", views.LoadModuleView.as_view(), name="LoadModuleView")
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -4,6 +4,13 @@ from django.views import View
 # Create your views here.
 
 class HomeView(View):
-    
+
     def get(self, request):
         return render(request, 'app/index.html')
+    
+class LoadModuleView(View):
+
+    def get(self, request, count):
+        return render(request, 'app/module.html', {'count': count})
+    
+# class
